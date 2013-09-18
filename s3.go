cc6454dbb4e0f5ad319ebe3cec20e9b5ff81ae67
Juan Valencia <jvalencia@sharethis.com>
2013-09-18 16:09:29 -0700

    initial commit of s3 funcs

--- s3.go
+++ s3.go
@@ -14,3 +14,21 @@ func GetS3() *s3.S3 {
 	}
 	return s3Conn
 }
+
+func UploadBuffer(bucket string, path string, data []byte) {
+	s := GetS3()
+        b := s.Bucket(bucket)
+        b.Put(path, data, "content-type", s3.Private)
+}
+
+func DownloadBuffer(bucket string, path string) []byte {
+	s := GetS3()
+        b := s.Bucket(bucket)
+	data, err := b.Get(path)
+        if err != nil {
+                return []byte{}
+        } else {
+                return data
+        }
+	return []byte{}
+}
---
+++ s3_test.go
@@ -0,0 +1,10 @@
+package uploadthis
+
+import (
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+func (s *MySuite) TestS3(c *C) {
+
+}
