d2ae61390b0bb0faab5d9d45b83d4d7aa02699f5
Juan Valencia <jvalencia@sharethis.com>
2013-09-19 14:26:41 -0700

    interim commit for s3 test

--- s3_test.go
+++ s3_test.go
@@ -2,9 +2,18 @@ package uploadthis
 
 import (
 	. "launchpad.net/gocheck"
-	"testing"
 )
 
-func (s *MySuite) TestS3(c *C) {
+type S3Suite struct{}
 
+var _ = Suite(&S3Suite{})
+
+func (s *S3Suite) TestS3(c *C) {
+	bucket := "pointabout"
+	path := "testS3"
+	data := []byte{'h', 'e', 'l', 'l', 'o'}
+	UploadBuffer(bucket, path, data)
+	returnData := DownloadBuffer(bucket, path)
+	c.Assert(string(returnData), Equals, string(data))
+	c.Assert("a", Equals, "a")
 }
