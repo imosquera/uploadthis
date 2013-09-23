35db129200ad76ab7c07265c6e656916abc1a8ec
Isaac Mosquera <imosquer@gmail.com>
2013-09-23 15:45:04 -0700

    more for debugging

--- hooks/prehooks_test.go
+++ hooks/prehooks_test.go
@@ -13,10 +13,5 @@ type MySuite2 struct{}
 var _ = Suite(&MySuite2{})
 
 //this test makes sure that the access key and secret keys are set
-func (s *MySuite2) TestAuthSet(c *C) {
-}
-
-//this test makes sure that are NOT set if one key is missing
-//and if it's not it wont set the keys on the global settings packages
-func (s *MySuite2) TestAuthNotSet(c *C) {
+func (s *MySuite2) TestAuthSet2(c *C) {
 }
