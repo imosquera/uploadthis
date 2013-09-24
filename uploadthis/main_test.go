78c1f82787cadd55cc409da8c04c9d01d110d978
Isaac Mosquera <imosquer@gmail.com>
2013-09-23 17:23:45 -0700

    updating hte packages

---
+++ uploadthis/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test2(t *testing.T) { TestingT(t) }
+
+type MySuite2 struct{}
+
+var _ = Suite(&MySuite2{})
+
+//this test makes sure that the access key and secret keys are set
+func (s *MySuite2) TestAuthSet2(c *C) {
+
+}
