4030006314f329e99eceff44a21d062c0d8858ee
Isaac Mosquera <imosquer@gmail.com>
2013-10-07 15:28:49 -0700

    adding more test coverage

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -1,17 +1,38 @@
 package monitor
 
 import (
+	"code.google.com/p/gomock/gomock"
+	"github.com/imosquera/uploadthis/util/mocks"
+	mockio "io/ioutil" //mock
 	. "launchpad.net/gocheck"
+	"os"
 	"testing"
 )
 
 // Hook up gocheck into the "go test" runner.
-func Test2(t *testing.T) { TestingT(t) }
+func TestInit(t *testing.T) { TestingT(t) }
 
-type MySuite2 struct{}
+type MonitorTest struct{}
 
-var _ = Suite(&MySuite2{})
+var _ = Suite(&MonitorTest{})
 
 //this test makes sure that the access key and secret keys are set
-func (s *MySuite2) TestUploadFilesInfo(c *C) {
+func (s *MonitorTest) TestGetUploadFiles(c *C) {
+	mockCtrl := gomock.NewController(c)
+	defer mockCtrl.Finish()
+
+	filePath := mocks.NewMockFileInfo(mockCtrl)
+	filePath.EXPECT().IsDir().Return(false)
+	filePath.EXPECT().Name().Return("mockfile")
+
+	mockio.MOCK().SetController(mockCtrl)
+	mockio.EXPECT().ReadDir("mockpath").Return([]os.FileInfo{filePath}, nil)
+
+	fileInfos := GetUploadFiles("mockpath")
+	thePath := ""
+	for _, fileInfo := range fileInfos {
+		thePath = fileInfo.Path
+	}
+	c.Assert(len(fileInfos), Equals, 1)
+	c.Assert(thePath, Equals, "mockpath/mockfile")
 }
