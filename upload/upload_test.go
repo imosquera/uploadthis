385284f9ddf79edc66256b9cb169f67526ff4e3c
Nazar Holovatchyk <nholovatchyk@lohika.com>
2013-10-24 18:08:39 +0300

    Fix test Story: upload should place files in date based structure such as /2013/01/01 https://www.pivotaltracker.com/story/show/59341796

--- upload/upload_test.go
+++ upload/upload_test.go
@@ -37,7 +37,7 @@ func (s *UploadSuite) TestGeneratePathPrefix(c *C) {
 
 func formatDate(t time.Time) string {
 	year, month, day := t.Date()
-	return fmt.Sprintf("%04d-%02d-%02d", year, month, day)
+	return fmt.Sprintf("%04d/%02d/%02d", year, month, day)
 }
 func (s *UploadSuite) TestS3Upload(c *C) {
 	mockCtrl := gomock.NewController(c)
