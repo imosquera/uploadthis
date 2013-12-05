44e26948097f723ba9938f0c88d7588568bc1708
Nazar Holovatchyk <nholovatchyk@lohika.com>
2013-12-05 18:10:10 +0200

    Story: file uploader should give a unique name to the file before being uploaded to s3 https://www.pivotaltracker.com/story/show/60974272

--- hooks/prehooks.go
+++ hooks/prehooks.go
@@ -4,9 +4,13 @@ import (
 	"compress/gzip"
 	"github.com/imosquera/uploadthis/commands"
 	"github.com/imosquera/uploadthis/util"
+	log "github.com/cihub/seelog"
 	"io"
-	"log"
 	"path"
+	"os"
+	"path/filepath"
+	"crypto/rand"
+	"encoding/hex"
 )
 
 //*********************
@@ -17,7 +21,7 @@ type GzipFileCompressor struct{}
 func (self *GzipFileCompressor) Compress(filepath string) (string, error) {
 	inFile, err := util.Fs.Open(filepath)
 	if err != nil {
-		log.Fatal("Error for file " + filepath + " " + err.Error())
+		log.Critical("Error for file " + filepath + " " + err.Error())
 	}
 
 	//create the gzip file
@@ -59,6 +63,72 @@ func (c CompressPrehook) Run() ([]string, error) {
 	for _, uploadFile := range c.UploadFiles {
 		uploadFile, _ := c.compressor.Compress(uploadFile)
 		newFiles = append(newFiles, uploadFile)
+	}
+	return newFiles, err
+}
+
+//**************
+// Renamer
+//**************
+type Renamer interface {
+	Rename(filepath string) (string, error)
+	SetProperties(...interface {})
+}
+
+type UniqueSuffixRenamer struct{
+	Renamer
+	suffixLength int
+}
+
+func (self *UniqueSuffixRenamer) Rename(filename string) (string, error) {
+	var extension = filepath.Ext(filename)
+	var name = filename[0:len(filename)-len(extension)]
+	suffix, _ := self.GenerateRandomString(self.suffixLength)
+	var newName = name + "-" + suffix + extension
+
+	err := os.Rename(filename, newName)
+	if err != nil {
+		log.Critical("Error for file " + filename + " " + err.Error())
+		util.LogPanic(err)
+	}
+	return newName, err
+}
+
+func (self *UniqueSuffixRenamer) GenerateRandomString(length int) (string, error) {
+	uuid := make([]byte, length)
+	_, err := rand.Read(uuid)
+	if err != nil {
+		log.Critical("Error generating random string " + err.Error())
+		util.LogPanic(err)
+		return "", err
+	}
+	return hex.EncodeToString(uuid), nil
+}
+
+type RenamePrehook struct {
+	*commands.Command
+	renamer Renamer
+}
+
+func NewRenamePrehook() *RenamePrehook {
+	return &RenamePrehook{
+		commands.NewFileStateCommand(),
+		&UniqueSuffixRenamer{suffixLength: 5},
+	}
+}
+
+//this function will rename the infile and
+//return a new file name
+func (self RenamePrehook) Run() ([]string, error) {
+	var err error
+	newFiles := make([]string, 0, len(self.UploadFiles))
+	for _, uploadFile := range self.UploadFiles {
+		newFile, the_err := self.renamer.Rename(uploadFile)
+		log.Info("Rename: ", uploadFile, " -> ", newFile)
+		if the_err != nil {
+			err = the_err
+		}
+		newFiles = append(newFiles, newFile)
 	}
 	return newFiles, err
 }
